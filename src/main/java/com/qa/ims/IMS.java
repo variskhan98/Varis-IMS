96699ed44d93b7e23c1d570ca2122cc8b071e615
variskhan98 <98025285+variskhan98@users.noreply.github.com>
2022-02-21 02:50:30 +0000

    Updated IMS
    Final code with everything implemented

--- src/main/java/com/qa/ims/IMS.java
+++ src/main/java/com/qa/ims/IMS.java
@@ -1,11 +1,12 @@
 package com.qa.ims;
 
+import com.qa.ims.controller.*;
+import com.qa.ims.persistence.dao.ItemDAO;
+import com.qa.ims.persistence.dao.OrderDAO;
+import com.qa.ims.persistence.domain.Order;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.qa.ims.controller.Action;
-import com.qa.ims.controller.CrudController;
-import com.qa.ims.controller.CustomerController;
 import com.qa.ims.persistence.dao.CustomerDAO;
 import com.qa.ims.persistence.domain.Domain;
 import com.qa.ims.utils.DBUtils;
@@ -16,12 +17,22 @@ public class IMS {
 	public static final Logger LOGGER = LogManager.getLogger();
 
 	private final CustomerController customers;
+	private final ItemController items;
+	private final OrderController orders;
+
 	private final Utils utils;
 
 	public IMS() {
 		this.utils = new Utils();
 		final CustomerDAO custDAO = new CustomerDAO();
 		this.customers = new CustomerController(custDAO, utils);
+
+		final ItemDAO itemDAO = new ItemDAO();
+		this.items = new ItemController(itemDAO,utils);
+
+
+		final OrderDAO orderDAO = new OrderDAO();
+		this.orders = new OrderController(orderDAO,utils);
 	}
 
 	public void imsSystem() {
@@ -50,10 +61,13 @@ public class IMS {
 				active = this.customers;
 				break;
 			case ITEM:
+				active = this.items;
 				break;
 			case ORDER:
+				active = this.orders;
 				break;
 			case STOP:
+				System.exit(-1);
 				return;
 			default:
 				break;
